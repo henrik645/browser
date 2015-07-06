609bbb227cc10850d06d9dd73f3a246092bc8d71
Henrik Vester <henrik645@gmail.com>
2015-07-06 11:53:47 +0200

    Fix bug with 'linebreak' tag
    Special calls would get corrupted at the call to this tag.

--- src/tags/linebreak.py
+++ src/tags/linebreak.py
@@ -2,7 +2,7 @@ def replace(msg, special_content):
     return '\n'
 
 def add_call(content, special_info):
-    return (special_info, {})
+    return ({}, special_info)
 
 function = {
     'replace': replace,
